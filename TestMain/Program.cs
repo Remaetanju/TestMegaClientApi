970b9e766de9d5cb39106fc8391292f017ad3196
Remaetanju <remibraizet@gmail.com>
2023-12-31 02:39:45 +0100

    initial commit

---
+++ TestMain/Program.cs
@@ -0,0 +1,44 @@
+﻿using CG.Web.MegaApiClient;
+namespace LCMedalpatcher
+{
+    internal class Patcher
+    {
+        [STAThread]
+        public static void Main()
+        {
+            MegaApiClient MyClient = new();
+
+
+            try
+            {
+                MyClient.LoginAnonymous();
+
+                Console.WriteLine(MyClient.IsLoggedIn);
+                Console.WriteLine(MyClient.IsLoggedIn.GetType().ToString());
+
+            }
+            catch (Exception ex)
+
+
+            {
+                Console.WriteLine(ex.Message);
+            }
+            try
+            {
+                MyClient.Login();
+
+                Console.WriteLine(MyClient.IsLoggedIn);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+            }
+
+
+            Console.Write("\t\tPress Any Key to leave...\n");
+            Console.ReadKey();
+        }
+    }
+}
