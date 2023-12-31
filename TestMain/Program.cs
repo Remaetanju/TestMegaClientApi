e0cb0de1f5870236e69495980f3afc3f476a8049
Remaetanju <45426037+Remaetanju@users.noreply.github.com>
2023-12-31 03:08:50 +0100

    Update Program.cs
    updated

--- TestMain/Program.cs
+++ TestMain/Program.cs
@@ -7,35 +7,17 @@ namespace LCMedalpatcher
         public static void Main()
         {
             MegaApiClient MyClient = new();
-
-
             try
             {
                 MyClient.LoginAnonymous();
 
                 Console.WriteLine(MyClient.IsLoggedIn);
-                Console.WriteLine(MyClient.IsLoggedIn.GetType().ToString());
-
             }
-            catch (Exception ex)
-
-
-            {
-                Console.WriteLine(ex.Message);
-            }
-            try
-            {
-                MyClient.Login();
-
-                Console.WriteLine(MyClient.IsLoggedIn);
-
-            }
+            
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
-
             }
-
 
             Console.Write("\t\tPress Any Key to leave...\n");
             Console.ReadKey();
